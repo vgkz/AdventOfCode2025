0bcc5c1aa56b61e875f2bed6685a4a74720cd132
faer <ferikson77@gmail.com>
2025-12-08 23:50:36 +0100

    solved part 2

--- day1/day1part2.hs
+++ day1/day1part2.hs
@@ -6,35 +6,38 @@ import qualified Data.Text.IO as TIO
 strToInt :: String -> Int
 strToInt = read
 
--- rotate dial to the left
-left :: Int -> Int -> Int
-left 0 y = y
-left x 0 = left (x-1) 99
-left x y = left (x-1) (y-1)
+-- f increments by 1, if 100 cycle back to 0, if -1 cycle back to 99
+turn :: (Int -> Int) -> Int -> Int
+turn f x | f x == 100 = 0
+         | f x == -1 = 99
+         | otherwise = f x
 
--- rotate dial to the right
-right :: Int -> Int -> Int
-right 0 y = y
-right x 99 = right (x-1) 0
-right x y = right (x-1) (y+1)
+-- return 1 if 0, else 0
+detect0 :: Int -> Int
+detect0 x = if x == 0
+            then 1
+            else 0
+
+-- turn dial in direction determined by f
+turnCycle :: (Int -> Int) -> Int -> Int -> (Int, Int)
+turnCycle f nturns pos = turnHelper (turn f) nturns pos 0
+                         where turnHelper :: (Int -> Int) -> Int -> Int -> Int -> (Int, Int)
+                               turnHelper g 0 pos count = (pos, count)
+                               turnHelper g nturns pos count = turnHelper g (nturns-1) (g pos) (count + detect0 pos) 
 
 -- move dial either left or right and return new position
-move :: String -> Int -> Int
-move (x:xs) y | x == 'L' = left (read xs) y 
-              | x == 'R' = right (read xs) y
+move :: String -> Int -> (Int, Int)
+move (x:xs) y | x == 'L' = turnCycle (subtract 1) (read xs) y 
+              | x == 'R' = turnCycle (+1) (read xs) y
 
 -- walk through rotations in list of rotations
 walker :: [String] -> Int -> Int -> Int
-walker [] y c = c 
-walker (x:xs) y c = walker xs y2 c2
-                    where y2 = move x y
-                          c2 = if y2 == 0
-                               then c+1
-                               else c
+walker [] y c = c + detect0 y 
+walker (x:xs) y c = walker xs y2 (c+c2)
+                    where (y2, c2) = move x y
 
 main :: IO ()
 main = do
     lines <- fmap T.lines (TIO.readFile "data.txt")
     let z = map T.unpack lines
     print $ walker z 50 0
-
