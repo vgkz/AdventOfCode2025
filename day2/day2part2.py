8326ecb6c59d734b466fb3033c4076f8fc4d9f64
faer <ferikson77@gmail.com>
2025-12-09 19:56:29 +0100

    rename files for day2 part 2

---
+++ day2/day2part2.py
@@ -0,0 +1,32 @@
+def checkValidity(inputId : str) -> bool:
+    idLen = len(inputId)
+    if (idLen % 2) != 0:
+        return True
+    else:
+        headHalf = inputId[:idLen//2]
+        tailHalf = inputId[idLen//2:]
+        return not (headHalf == tailHalf)
+
+def strToRange(inputString : str) -> range:
+    init, end = inputString.split("-")
+    return range(int(init), int(end)+1)
+
+def flatten(inputList : list[list])->list:
+    return [nestedval for nestedlist in inputList for nestedval in nestedlist]
+
+def extractIds(inputString : str) -> list[str]:
+    strRanges = inputString.split(",")
+    nestedValues = [list(strToRange(r)) for r in strRanges]
+    values = flatten(nestedValues) 
+    return [str(v) for v in values]
+
+def main():
+    with open("data.txt") as h:
+        data = h.read()
+    ids = extractIds(data)
+    incorrectIds = [i for i in ids if not checkValidity(i)]
+    print(sum([int(i) for i in incorrectIds]))
+
+
+if __name__ == "__main__":
+    main()
