e1461483422316987bee7b5ee2e0b661b553ffdf
faer <ferikson77@gmail.com>
2025-12-10 21:50:10 +0100

    working test solution day 3 part 2

--- day3/main.R
+++ day3/main.R
@@ -1,7 +1,7 @@
-input <- c("987654321111111",
-           "811111111111119",
-           "234234234234278",
-           "818181911112111")
+test_input <- c("987654321111111",
+                "811111111111119",
+                "234234234234278",
+                "818181911112111")
 
 split_bank <- function(bank) {
   batteries <- unlist(strsplit(bank, ""))
@@ -17,8 +17,23 @@ max_joltage <- function(batteries) {
          remaining_batteries[i_snd_max_joltage_battery])
 }
 
+max_joltage2 <- function(remaining_bats, selected_bats, n_bats_left) {
+  if (n_bats_left == 0) {
+    cur_n_bats <- length(remaining_bats)
+    i_max_bat <- which.max(remaining_bats[1:(cur_n_bats - n_bats_left)])
+    paste0(c(selected_bats, remaining_bats[i_max_bat]), collapse = "")
+  } else {
+    cur_n_bats <- length(remaining_bats)
+    i_max_bat <- which.max(remaining_bats[1:(cur_n_bats - n_bats_left)])
+    cur_remaining_bats <- remaining_bats[(i_max_bat + 1):cur_n_bats]
+    cur_selected_bats <- c(selected_bats, remaining_bats[i_max_bat])
+    max_joltage2(cur_remaining_bats, cur_selected_bats, n_bats_left - 1)
+  }
+}
+
 data <- scan("data.txt", what = "character")
 
-Map(function(x) (max_joltage(split_bank(x))), data) |>
-  as.integer() |>
-  sum()
+Map(function(x) (max_joltage2(split_bank(x), c(), 11)), test_input) |>
+    as.numeric() |>
+    sum() |>
+    format(scientific = FALSE)
