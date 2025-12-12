0c0448a0924146a4d33b6d2b9cc8c68500bc914b
faer <ferikson77@gmail.com>
2025-12-12 23:22:38 +0100

    solved: day 4 part 4

--- day4/main.lua
+++ day4/main.lua
@@ -1,4 +1,5 @@
 function splitString(s)
+    -- split string into table of characters
     local res = {}
     for i = 1, string.len(s) do
         res[i] = string.sub(s, i, i)
@@ -7,6 +8,7 @@ function splitString(s)
 end
 
 function splitTable(t)
+    -- split table of strings into table of table of characters
     local res = {}
     for i, v in ipairs(t) do
         res[i] = splitString(v)
@@ -15,6 +17,7 @@ function splitTable(t)
 end
 
 function filter(t, min, max)
+    -- filter values in table that are outside range (min, max)
     local newt = {}
     for i, v in ipairs(t) do
         if v>min and v<max
@@ -26,6 +29,7 @@ function filter(t, min, max)
 end
 
 function countNeighbors(t, iy, ix, max_x, max_y)
+    -- count how many neighbor rolls a position has
     local nxs = {ix-1, ix, ix+1}
     local nys = {iy-1, iy, iy+1}
     nxs = filter(nxs, 0, (max_x+1))
@@ -44,6 +48,7 @@ function countNeighbors(t, iy, ix, max_x, max_y)
 end
 
 function tableLength(t)
+    -- return how many entries a table has 
     local count = 0
     for _ in pairs(t) do
         count = count + 1
@@ -52,6 +57,7 @@ function tableLength(t)
 end
 
 function countAccessableRolls(t)
+    -- count how many rolls in table are accessable (neightbors<4)
     local max_x = tableLength(t[1])
     local max_y = tableLength(t)
     local count = 0
@@ -71,6 +77,7 @@ function countAccessableRolls(t)
 end
 
 function findAccessableRolls(t)
+    -- return coordinates of rolls that are accessable
     local max_x = tableLength(t[1])
     local max_y = tableLength(t)
     local accessable_rolls = {}
@@ -90,59 +97,37 @@ function findAccessableRolls(t)
 end
 
 function removeAccessableRolls(t, accessable_rolls)
+    -- remove rolls at coordinates in accessable_rolls, return new table
     for i, coord in ipairs(accessable_rolls) do
         t[coord[2]][coord[1]] = "."
     end
     return t
 end
 
-
-test_input = {"..@@.@@@@.",
-              "@@@.@.@.@@",
-              "@@@@@.@.@@",
-              "@.@@@@..@.",
-              "@@.@@@@.@@",
-              ".@@@@@@@.@",
-              ".@.@.@.@@@",
-              "@.@@@.@@@@",
-              ".@@@@@@@@.",
-              "@.@.@@@.@."}
-
-test_input = splitTable(test_input)
-
--- solve part 1
+-- solve puzzle
 do
+    -- read data
     local file = "data.txt"
     local lines = {}
     for line in io.lines(file) do
         lines[#lines+1] = line
     end
     local puzzle_input = splitTable(lines)
-    local count = countAccessableRolls(puzzle_input)
-    print(count)
-end
+    -- solve part 1
+    local count_part1 = countAccessableRolls(puzzle_input)
+    print(count_part1)
 
--- solve part 2
-do
-    --local file = "data.txt"
-    --local lines = {}
-    --for line in io.lines(file) do
-        --lines[#lines+1] = line
-    --end
-    --local puzzle_input = splitTable(lines)
+    -- solve part 2
     local in_loop = true
-    local count = 0
+    local count_part2 = 0
     while in_loop == true do
-        local accs_rolls = findAccessableRolls(test_input)
-        count = count + tableLength(accs_rolls)
-        test_input = removeAccessableRolls(test_input, accs_rolls)
+        local accs_rolls = findAccessableRolls(puzzle_input)
+        count_part2 = count_part2 + tableLength(accs_rolls)
+        puzzle_input = removeAccessableRolls(puzzle_input, accs_rolls)
         if tableLength(accs_rolls) == 0
             then
                 in_loop = false
         end
     end
-    for i, row in ipairs(test_input) do
-        print(table.concat(row, ""))
-    end
-    print(count)
+    print(count_part2)
 end
